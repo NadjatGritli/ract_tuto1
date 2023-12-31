a844430af70d2c466c35748f4a0fe5aef3aa1b8e
nadjat <grinadjat1996@gmail.com>
2023-12-31 08:52:28 +0100

    start netninjaTuto ReactRouterInDepth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
 
 // pages
 import Home from './pages/Home'
@@ -6,30 +11,41 @@ import About from './pages/About'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
 
+// Layout page
+
+import Layout from "./layout/layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products/:id" element={<ProductDetails />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/test_inline_component" element={(
+        <div>
+          <h3>
+            test hee
+          </h3>
+        </div>
+      )} />
+    </Route>
+  )
+)
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <nav>
-          <h1>The Ninja Clothing Company</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+      <RouterProvider router={router}></RouterProvider>
+    // <div className="App">
+    //   <BrowserRouter>
+    //     <main>
+    //       <div className="main_container">
+    //         <Routes>
+
+    //         </Routes>
+    //       </div>
+    //     </main>
+    //   </BrowserRouter>
+    // </div>
   )
 }
 
